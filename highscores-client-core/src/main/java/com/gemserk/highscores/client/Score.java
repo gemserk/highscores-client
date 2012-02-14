f44f76add1e27ab98d37cdb40fe775429108e52b
Ruben Garat <ruben.garat@gemserk.com>
2012-02-14 19:39:45 -0200

    added constructor to score

--- highscores-client-core/src/main/java/com/gemserk/highscores/client/Score.java
+++ highscores-client-core/src/main/java/com/gemserk/highscores/client/Score.java
@@ -4,30 +4,40 @@ public class Score {
 	String username;
 	String name;
 	long score;
-	
-	
+
+	public Score(String username, String name, long score) {
+		this.username = username;
+		this.name = name;
+		this.score = score;
+	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public long getScore() {
 		return score;
 	}
+
 	public void setScore(long score) {
 		this.score = score;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Score[" + name + " - " + score + "]"; 
+		return "Score[" + name + " - " + score + "]";
 	}
-	
+
 }
