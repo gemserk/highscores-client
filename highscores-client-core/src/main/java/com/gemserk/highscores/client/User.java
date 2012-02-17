2578a415895ef6f7f5ae631bdbe6d274f55a3376
Ruben Garat <ruben.garat@gemserk.com>
2012-02-17 14:24:10 -0200

    added guest info to user

--- highscores-client-core/src/main/java/com/gemserk/highscores/client/User.java
+++ highscores-client-core/src/main/java/com/gemserk/highscores/client/User.java
@@ -5,11 +5,13 @@ public class User {
 	long userId;
 	String name;
 	String privatekey;
+	boolean guest;
 
-	public User(long userId, String name, String privatekey) {
+	public User(long userId, String name, String privatekey, boolean guest) {
 		this.userId = userId;
 		this.name = name;
 		this.privatekey = privatekey;
+		this.guest = guest;
 	}
 
 	public long getUserId() {
@@ -22,5 +24,9 @@ public class User {
 
 	public String getPrivatekey() {
 		return privatekey;
+	}
+	
+	public boolean isGuest() {
+		return guest;
 	}
 }
